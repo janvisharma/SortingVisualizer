839bafd4535a9760654fd7d413a3b2b304d24364
janvisharma <janvi@connect.hku.hk>
2020-06-13 01:40:04 +0530

    adds insertion sort

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './SortingVisualizer.css';
 import Button from 'react-bootstrap/Button';
-import bubbleSort from '../SortingAlgorithms/SortingAlgorithms.js';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 class SortingVisualizer extends React.Component{
     constructor(props) { 
         super(props);
@@ -36,10 +35,75 @@ class SortingVisualizer extends React.Component{
                 arrayBars[j+1].style.backgroundColor =  '#d100d1';//'#3da4ab';
             }
             this.setState({array})
-            arrayBars[array.length-i-1].style.backgroundColor = '#8900f2';//'#bbf1c8';
+            arrayBars[array.length-i-1].style.backgroundColor = '#e0aaff';//'#bbf1c8';
         }
 
         for(i=0;i<180;i++){
+            await new Promise(resolve => setTimeout(resolve, 0.5));
+            arrayBars[i].style.backgroundColor = '#3da4ab';
+        }
+        this.setState({generateButton: false})
+
+    }
+    async selectionSort(){
+        var array = this.state.array
+        var arrayBars = document.getElementsByClassName('bar')
+
+        var small, pos=0, i, k, temp
+
+        for(i=0;i<180;i++){
+            small = array[i]
+            pos = i            
+            await new Promise(resolve => setTimeout(resolve, 5));
+            arrayBars[i].style.backgroundColor = '#2ec4b6'
+            for(k=i+1;k<180;k++){
+                await new Promise(resolve => setTimeout(resolve, 5));
+                arrayBars[k].style.backgroundColor = '#2ec4b6'
+                if(array[k]<small){
+                    small = array[k]
+                    pos = k
+                }
+                await new Promise(resolve => setTimeout(resolve, 5));
+                arrayBars[k].style.backgroundColor = '#8338ec'
+            }
+        
+            temp = array[i]
+            array[i] = small
+            array[pos] = temp
+            this.setState({array})
+            await new Promise(resolve => setTimeout(resolve, 7));
+            arrayBars[pos].style.backgroundColor = '#ffbe0b'
+        }
+        this.setState({array})
+    }
+
+    async insertionSort() {
+        this.setState({generateButton: true}) // disable the generateArrayButton
+        var array = this.state.array // current array to be sorted 
+        var arrayBars = document.getElementsByClassName('bar'); // for animations
+
+        var i, j;
+        for(i=0;i<180;i++){
+            var temp = array[i];
+            await new Promise(resolve => setTimeout(resolve, 5));
+            arrayBars[i].style.backgroundColor = '#ff1654';
+            j = i-1;
+            while(j>=0 && array[j]>temp){
+                await new Promise(resolve => setTimeout(resolve, 5));
+                arrayBars[j].style.backgroundColor = '#ff1654';
+                array[j+1] = array[j];
+                await new Promise(resolve => setTimeout(resolve, 1));
+                arrayBars[j].style.backgroundColor = '#7678ed'//'#ffa6c1';
+                j = j - 1;
+            }
+            arrayBars[i].style.backgroundColor = '#7678ed' //'#ffa6c1';
+
+            array[j+1] = temp;
+            this.setState({array})
+        }
+        
+        for(i=0;i<180;i++){
+            await new Promise(resolve => setTimeout(resolve, 0.5));
             arrayBars[i].style.backgroundColor = '#3da4ab';
         }
         this.setState({generateButton: false})
@@ -70,8 +134,8 @@ class SortingVisualizer extends React.Component{
                 <Button style={{margin: '5px'}} variant="outline-primary" onClick={this.bubbleSorthelper.bind(this)}>Bubble Sort</Button>{' '}
                 <Button style={{margin: '5px'}} variant="outline-primary">Merge Sort</Button>{' '}
                 <Button style={{margin: '5px'}} variant="outline-primary">Quick Sort</Button>{' '}
-                <Button style={{margin: '5px'}} variant="outline-primary">Insertion Sort</Button>{' '}
-                <Button style={{margin: '5px'}} variant="outline-primary">Selection Sort</Button>{' '}
+                <Button style={{margin: '5px'}} variant="outline-primary" onClick={this.insertionSort.bind(this)}>Insertion Sort</Button>{' '}
+                <Button style={{margin: '5px'}} variant="outline-primary"onClick={this.selectionSort.bind(this)}>Selection Sort</Button>{' '}
                 </div>
             </Container>
         );
